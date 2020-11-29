43044dae384fa1768c0cf358eeaeac17d882b4a8
Manish <manish@Manishs-MacBook-Air.local>
2020-11-29 18:44:53 +0530

    adding test cased for register and who method

--- src/test/java/com/budget/app/controller/UserControllerTest.java
+++ src/test/java/com/budget/app/controller/UserControllerTest.java
@@ -2,34 +2,35 @@ package com.budget.app.controller;
 
 import com.budget.app.entity.User;
 import com.budget.app.model.Response;
-import com.budget.app.model.user.LoginRequest;
 import com.budget.app.model.user.LoginResponse;
-import com.budget.app.model.user.UserInfoOnLogin;
+import com.budget.app.responseMessage.ResponseMessage;
 import com.budget.app.service.UserService;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.io.IOException;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.util.Optional;
 
 @SpringBootTest
 public class UserControllerTest {
@@ -42,81 +43,77 @@ public class UserControllerTest {
     @Autowired
     private WebApplicationContext context;
 
-    ObjectMapper MAPPER = new ObjectMapper();
+    @Value("${jwt.test.token}")
+    private String authToken;
+
+    private Optional<User> user;
+
+    private static ObjectMapper MAPPER;
+
+    @BeforeAll
+    public static void beforeAll() {
+
+        MAPPER = new ObjectMapper();
+        MAPPER.registerModule(new JavaTimeModule());
+    }
 
     @BeforeEach
     public void setup() {
+
+        user = Optional.of(new User("test.user@gmail.com", "testabc", "Test", "User", LocalDate.parse("1990-01-01"), "Male", "ROLE_USER"));
         mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
     }
 
     protected String mapToJson(Object obj) throws JsonProcessingException {
-        ObjectMapper mapper = new ObjectMapper();
-        return mapper.writeValueAsString(obj);
+        return MAPPER.writeValueAsString(obj);
     }
 
     protected <T> T mapFromJson(String json, Class<T> clazz)
             throws JsonParseException, JsonMappingException, IOException {
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        return objectMapper.readValue(json, clazz);
+        return MAPPER.readValue(json, clazz);
     }
 
     @Test
     public void registerUserTest() throws Exception {
-        Response response = new Response(HttpStatus.OK.value(), "User has been successfully registered", LocalDateTime.now());
 
-//        User user = new User("abc123@gmail.com", "testabc", "Test", "User", LocalDate.parse("1995-12-15"), "Male", "ROLE_USER");
-
-        String request = "{\n" +
-                "\t\"firstName\": \"Test\",\n" +
-                "\t\"lastName\": \"User\",\n" +
-                "\t\"email\": \"test123.user@gmail.com\",\n" +
-                "\t\"password\": \"random\",\n" +
-                "\t\"gender\": \"Male\",\n" +
-                "\t\"dateOfBirth\": \"1995-12-15\"\n" +
-                "}";
-
-        System.out.println("object: " + request);
         MvcResult mvcResult =  mockMvc
                 .perform(MockMvcRequestBuilders.post("/user/register")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(request))
+                .content(mapToJson(user.get())))
                 .andReturn();
 
         int status = mvcResult.getResponse().getStatus();
 
         String content = mvcResult.getResponse().getContentAsString();
-//        Response mvcResponse = mapFromJson(content, Response.class);
-//        System.out.println("mvcResponse: " +  mvcResponse);
+        Response mvcResponse = mapFromJson(content, Response.class);
 
         Assertions.assertEquals(HttpStatus.OK.value(), status);
+        Assertions.assertEquals(mvcResponse.getMessage(), ResponseMessage.USER_CREATION_SUCCESS.toString());
+        Assertions.assertEquals(mvcResponse.getStatusCode(), HttpStatus.OK.value());
     }
 
     @Test
-    public void loginUserTest() throws Exception {
+    public void authenticateUserTest() throws Exception {
 
-        LoginRequest loginRequest = new LoginRequest("test.user@gmail.com", "random");
+        Mockito
+                .when(userService.findUserByEmail("test.user@gmail.com"))
+                .thenReturn(user);
 
-        UserInfoOnLogin userInfo = new UserInfoOnLogin(1, "Test", "ROLE_USER");
-        LoginResponse loginResponse = new LoginResponse();
-        loginResponse.setAuthToken("abcd");
-        loginResponse.setUser(userInfo);
-
-//        Mockito
-//                .when(userService.findUserByEmail("test.user@gmail.com"))
-//                .thenReturn(new LoginResponse());
-
-        System.out.println("login request: " + mapToJson(loginRequest));
         MvcResult mvcResult = mockMvc
-                .perform(MockMvcRequestBuilders.post("/user/login")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(mapToJson(loginRequest)))
+                .perform(MockMvcRequestBuilders.get("/user/who")
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + authToken))
                 .andReturn();
 
         int status = mvcResult.getResponse().getStatus();
+
         String content = mvcResult.getResponse().getContentAsString();
 
+        LoginResponse result = mapFromJson(content, LoginResponse.class);
+
         Assertions.assertEquals(HttpStatus.OK.value(), status);
+        Assertions.assertEquals(result.getUser().getFirstName(), "Test");
+        Assertions.assertEquals(result.getUser().getRole(), "ROLE_USER");
 
     }
 
